cf7a66d1cec81fa83cea3b27a55b03b55d3a9b58
Nirank <jawalenirank98@gmail.com>
2023-01-13 13:20:19 +0530

    CHal Submit Ho Ja BHAi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,18 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+
+        public int product(int x, int y){ return x*y; }
+        public int product(int x, int y, int z) { return x*y*z; }
+        public double product(double x, double y) { return x*y; }
+
+    }
     public static void main( String [] args) {
         Product p = new Product();
-        int x = p.product(3, 5);
-        int y = p.product(2, 3, 4);
-        double z = p.product(2.0, 3.0);
+        p.product(3, 5);
+        p.product(2, 3, 4);
+        p.product(2.0, 3.0);
     }
-
-}
-class Product{
-
-    public int product(int x, int y){ return x*y; }
-    public int product(int x, int y, int z) { return x*y*z; }
-    public double product(double x, double y) { return x*y; }
 
 }
